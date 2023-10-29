fea3ff2e2d5dcad08ab232734d75109feb7946c1
Siegmeyer0of0Catarina <siegmeyerofcatarina0x@gmail.com>
2023-10-29 17:50:49 +0300

    v3
    Programme method development, final version

--- Lab4/Lab4/Program.cs
+++ Lab4/Lab4/Program.cs
@@ -2,5 +2,23 @@
 
 class Program
 {
+    static void Main()
+    {
+        // Начальное количество амеб
+        int startingAmoebas = 1;
 
+        // Время в минутах
+        int timeInMinutes = 60;
+
+        // Расчет количества амеб через указанное время
+        int finalAmoebas = startingAmoebas * (int)Math.Pow(2, timeInMinutes);
+
+        Console.WriteLine("Через {0} минут в банке будет {1} амеб.", timeInMinutes, finalAmoebas);
+
+        // Дополнительный расчет для случая, когда в банку посадили 2 амебы
+        int startingAmoebas2 = 2;
+        int finalAmoebas2 = startingAmoebas2 * (int)Math.Pow(2, timeInMinutes);
+
+        Console.WriteLine("Если в банку посадить 2 амебы, через {0} минут будет {1} амеб.", timeInMinutes, finalAmoebas2);
+    }
 }
